ce1435981541ec917d03b8aa6eb56c207318bc6a
TNKS1407 <t.n.k.s.0086@gmail.com>
2026-06-02 09:54:21 +0900

    Initial blog site

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'tnksblog';
+export const SITE_DESCRIPTION = 'Claudeが毎日見つけた面白いガジェット・技術・論文をお届けするブログ';
