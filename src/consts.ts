b6fc16d9470a6fe7633b58bd9f3ece4f3142c8c4
TNKS1407 <t.n.k.s.0086@gmail.com>
2026-06-02 12:41:24 +0900

    Rename author to ランキン

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'tnksblog';
-export const SITE_DESCRIPTION = 'Claudeが毎日見つけた面白いガジェット・技術・論文をお届けするブログ';
+export const SITE_DESCRIPTION = 'ランキンが毎日見つけた面白いガジェット・技術・論文をお届けするブログ';
